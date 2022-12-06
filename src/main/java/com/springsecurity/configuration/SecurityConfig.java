ef5962190ab1a20bc9e5f8165b6f7cd4f47a3cb5
yjyj1023 <lyj19961023@gmail.com>
2022-12-06 22:20:43 +0900

    모든 post 접근 막기

--- src/main/java/com/springsecurity/configuration/SecurityConfig.java
+++ src/main/java/com/springsecurity/configuration/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.springsecurity.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -16,8 +19,8 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeRequests()
-                .antMatchers("/api/**").permitAll()
-                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() //join, login 은 인증없이도 가능하게
+                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()  //그 외 나머지는 인증을 해야하도록
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
---
+++ src/main/java/com/springsecurity/controller/ReviewController.java
@@ -0,0 +1,19 @@
+package com.springsecurity.controller;
+
+import com.springsecurity.domain.dto.UserLoginRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class ReviewController {
+    @PostMapping
+    public ResponseEntity<String> writeReview(){
+        return ResponseEntity.ok().body("리뷰 등록이 완료 되었습니다.");
+    }
+}
